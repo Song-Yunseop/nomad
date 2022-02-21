bf87b8b0b8e0103822ed855954d341dcb9f9365f
Song-Yunseop <junsub8044@naver.com>
2022-02-21 18:49:21 +0900

    2022/02/21

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const a = 10;
 const b =  2;
+let myName = "song";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+console.log("hello " + myName)
 
+myName = "yun";
+
+console.log("your new name is " + myName);
 
